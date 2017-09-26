07318360100dbfdc485b57092b92342d3d8eed06
D Ther <dther@opslab.space>
2017-09-26 12:52:49 +0630

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# xml-merger
+
+`xml-merger` multiple xml files into one file.
+
+
+## Usage
+
+```
+$ xml-merger *.xml > merged.xml
+$ xml-merger -o merged.xml x.xml y.xml z.xml
+```
+
+
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+type XmlReport struct {
+	XMLName   xml.Name
+	XML       string       `xml:",innerxml"`
+	Name      string       `xml:"name,attr"`
+	Time      float64      `xml:"time,attr"`
+	Tests     uint64       `xml:"tests,attr"`
+	Failures  uint64       `xml:"failures,attr"`
+	XMLBuffer bytes.Buffer `xml:"-"`
+}
+
+var usage = `Usage: xml-merger [options] [files]
+Options:
+  -o  Merged report filename`
+
+func main() {
+	flag.Usage = func() {
+		fmt.Println(usage)
+	}
+	outputFileName := flag.String("o", "", "merged report filename")
+	flag.Parse()
+	files := flag.Args()
+	printReport := *outputFileName == ""
+
+	if len(files) == 0 {
+		flag.Usage()
+		return
+	}
+
+	var mergedReport XmlReport
+	startedReading := false
+	fileCount := 0
+
+	for _, fileName := range files {
+		var report XmlReport
+		in, err := ioutil.ReadFile(fileName)
+
+		if err != nil {
+			panic(err)
+		}
+
+		err = xml.Unmarshal(in, &report)
+
+		if err != nil {
+			panic(err)
+		}
+
+		if report.XMLName.Local == "testsuite" {
+			panic(errors.New("Reports with a root <testsuite> are not supported"))
+		}
+
+		if startedReading && report.Name != mergedReport.Name {
+			panic(errors.New("All reports must have the same <testsuites> name"))
+		}
+
+		startedReading = true
+		fileCount++
+		mergedReport.XMLName = xml.Name{Local: "testsuites"}
+		mergedReport.Name = report.Name
+		mergedReport.Time += report.Time
+		mergedReport.Tests += report.Tests
+		mergedReport.Failures += report.Failures
+		mergedReport.XMLBuffer.WriteString(report.XML)
+	}
+
+	mergedReport.XML = mergedReport.XMLBuffer.String()
+	mergedOutput, _ := xml.MarshalIndent(&mergedReport, "", "  ")
+
+	if printReport {
+		fmt.Println(string(mergedOutput))
+	} else {
+		err := ioutil.WriteFile(*outputFileName, mergedOutput, 0644)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Merged " + strconv.Itoa(fileCount) + " reports to " + *outputFileName)
+	}
+}
